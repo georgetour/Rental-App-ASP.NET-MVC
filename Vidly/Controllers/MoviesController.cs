8ca3f14d9bf18dfc93c3b01ddac0ba66d16ce0ab
George Tourtsinakis <george@besmartbesimple.com>
2018-03-06 14:25:26 +0200

    Action Results and Custom Routing
    - ASP.NET MVC fundamentals
    Action Results, Action Parameters, Custom Routing and Attribute Routing
    - Added bootsrap theme Lumen for v.3 of Bootstrap

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -9,11 +9,38 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
+        
         public ActionResult Random()
         {
-            var movie = new Movie() {Name ="Shrek" };
-            return View(movie);
+            return RedirectToAction("Index","Home",new { page=1, sortBy ="name" });
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id="+id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex,string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2})}")]
+        public ActionResult ByReleaseDate(int year,byte month)
+        {
+            return Content(year +"/" +month);
         }
     }
 }
