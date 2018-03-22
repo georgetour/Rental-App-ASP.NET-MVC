f09a4dc4c6ac44e63a49393398a2357457fc96c9
George Tourtsinakis <george@besmartbesimple.com>
2018-03-22 14:55:54 +0200

    Add validation to our movie form
    - Add validation to our movie form
    All fields are required
    For stock we can't put 0 since the number must be between 1-20(Range data annotation)
    - Add client side validation
    - Protect the form using anti forgery token

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,13 +9,24 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(50)]
         public string Name { get; set; }
+
+        [Required(ErrorMessage ="Release Date filed is required.")]
         public DateTime ReleaseDate { get; set; }
+
         public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Range (1,10)]
         public byte Stock { get; set; }
+
         public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required(ErrorMessage = "Genre field is required.")]
         public int GenreId { get; set; }
 
     }
