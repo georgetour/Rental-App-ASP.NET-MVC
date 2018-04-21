89b19fe85a2468e8dc694fbf683c0438f19215f8
George Tourtsinakis <george@besmartbesimple.com>
2018-04-21 13:54:24 +0300

    Update guide

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -6,6 +6,8 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Runtime.Caching;
+
 
 namespace Vidly.Controllers
 {
@@ -30,7 +32,12 @@ namespace Vidly.Controllers
         [Authorize(Roles = RoleName.CanManageMovies)]
         public ActionResult Index()
         {
-           
+            if (MemoryCache.Default["Genres"]== null)
+            {
+                MemoryCache.Default["Genres"] = _context.Genres.ToList();
+            }
+            var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
+
             return View();
         }
 
