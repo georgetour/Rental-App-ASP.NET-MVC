a29bb537f03c11a62ddb00eb3f27789a0e77c76a
George Tourtsinakis <george@besmartbesimple.com>
2018-04-24 18:18:14 +0300

    Add API for Rental

---
+++ Vidly/Controllers/Api/RentalsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Http;
+using Vidly.Models;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.Api
+{
+    public class RentalsController : ApiController
+    {
+
+        private ApplicationDbContext _context;
+
+        //data from database
+        public RentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult CreateRental(RentalDto rentalDto)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
