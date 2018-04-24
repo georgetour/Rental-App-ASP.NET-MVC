014e5c8b5e8d60d7b74bfbf65a1944a790c22c4d
George Tourtsinakis <george@besmartbesimple.com>
2018-04-24 18:23:02 +0300

    Add Rental table

---
+++ Vidly/Models/Rental.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+
+        public Customer customer { get; set; }
+
+        public Movie movie { get; set; }
+
+        //What date the movie was rent
+        public DateTime DateRent { get; set; }
+
+        //What date the movie was returned
+        public DateTime? DateReturned { get; set; }
+
+
+    }
+}
